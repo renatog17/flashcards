ca0c1c00d49ae585f8763d99ba226d767dd7eacc
Renato Oliveira <renatog.oliveira17@gmail.com>
2025-04-12 15:54:44 -0300

    id added on ReadFlashCardDTO

--- src/main/java/com/renato/flashcards/flashcards_api/controller/dto/ReadFlashCardDTO.java
+++ src/main/java/com/renato/flashcards/flashcards_api/controller/dto/ReadFlashCardDTO.java
@@ -3,11 +3,11 @@ package com.renato.flashcards.flashcards_api.controller.dto;
 import com.renato.flashcards.flashcards_api.domain.FlashCard;
 
 public record ReadFlashCardDTO(
-		String term, String definition, String example,
+		Long id, String term, String definition, String example,
 		Long idDeck
 		) {
 
 	public ReadFlashCardDTO(FlashCard flashCard) {
-		this(flashCard.getTerm(), flashCard.getDefinition(), flashCard.getExample(), flashCard.getDeck().getId());
+		this(flashCard.getId(), flashCard.getTerm(), flashCard.getDefinition(), flashCard.getExample(), flashCard.getDeck().getId());
 	}
 }
