6ebf8b196a816768d13adb65e9ab362b703d1b1c
Renato Oliveira <renatog.oliveira17@gmail.com>
2025-04-13 16:55:02 -0300

    cors e properties ajustados

--- src/main/java/com/renato/flashcards/flashcards_api/config/CorsConfig.java
+++ src/main/java/com/renato/flashcards/flashcards_api/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins("http://191.252.38.148")//esse é o endereço para produção, para local utilize a porta
+                    //3000, verificar como fazer isso com os arquivos de configuração
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*");
             }
